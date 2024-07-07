fc5b34c308114e6b32b602a99ae09f156e60c45f
mohamedAdel <ma1704417@gmail.com>
2024-07-07 19:38:27 +0300

    finish thank you page and add vousher image

--- lib/Features/checkout/presentation/views/widgets/thank_you_card.dart
+++ lib/Features/checkout/presentation/views/widgets/thank_you_card.dart
@@ -13,7 +13,7 @@ class ThankYouCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.only(top: 50+16,left: 16 ,right: 16),
+      padding: const EdgeInsets.only(top: 50 , left: 16, right: 16),
       width: double.infinity,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(20),
@@ -31,25 +31,63 @@ class ThankYouCard extends StatelessWidget {
             textAlign: TextAlign.center,
             style: Styles.style20,
           ),
-          const SizedBox(height: 40,),
-          const CustomPaymentInfo(title: "Date",data: "01/24/2023",),
-          const SizedBox(height: 20,),
-          const CustomPaymentInfo(title: "Time",data: "10:15 AM",),
-          const SizedBox(height: 20,),
-          const CustomPaymentInfo(title: "To",data: "Sam Louis",),
+          const SizedBox(
+            height: 40,
+          ),
+          const CustomPaymentInfo(
+            title: "Date",
+            data: "01/24/2023",
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const CustomPaymentInfo(
+            title: "Time",
+            data: "10:15 AM",
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const CustomPaymentInfo(
+            title: "To",
+            data: "Sam Louis",
+          ),
           const Divider(
             height: 60,
             thickness: 2,
             color: Color.fromRGBO(199, 199, 199, 1),
           ),
           const TotalPrice(title: "Total", value: r"50.97$"),
-          const SizedBox(height: 40,),
+          const SizedBox(
+            height: 40,
+          ),
           const CustomCardInfo(),
-
+          const Spacer(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              SvgPicture.asset("assets/images/voucher.svg"),
+              Container(
+                width: 120,
+                height: 60,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  border: Border.all(color: Colors.green, width: 1),
+                ),
+                child: Center(
+                  child: Text(
+                    "PAID",
+                    style: Styles.style24.copyWith(
+                      color: const Color.fromRGBO(52, 182, 83, 1),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: ((MediaQuery.sizeOf(context).height * .23 + 20)/2)-30),
         ],
       ),
-
     );
   }
 }
-
